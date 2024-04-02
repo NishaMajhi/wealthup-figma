6c7e100338c4cbd0b8f2045d9f60a203c5eae26d
NishaMajhi <nishamajhi0@gmail.com>
2024-04-02 15:13:03 +0530

    first commit

---
+++ src/components/button/button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Buttons() {
+  return (
+    <button className="bg-[#FB7306] shadow-[1px_2.5px_0px_rgb(0,0,0,0.3)] w-max px-10 py-2 rounded-full font-semibold text-[32px] ">
+      Get Started
+    </button>
+  );
+}
